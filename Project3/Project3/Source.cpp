e9beaac72936a9f9c0b88fa464808752e1dfe8e7
Goshino <44379690+Goshino@users.noreply.github.com>
2018-10-30 11:20:32 -0700

    Update Source.cpp

--- Project3/Project3/Source.cpp
+++ Project3/Project3/Source.cpp
@@ -10,9 +10,16 @@ using namespace std;
 
 //Public variables for input and game state
 bool running = true;
+int cmd;
+
 int roomTrack = 0;
+static string pName;
 int pLevel;
-int cmd; 
+int pExp;
+int pExpUp;
+
+	//Public variables for functions
+bool characterConfirm = false;
 
 
 //Prototyping functions, so there won't be chronological conflict
@@ -23,7 +30,8 @@ void beginningAdventure();
 void beginningMenu();
 void bossEncounter();
 void dungeonDepth();
-
+void characterInitialize();
+void confirmCharacter();
 
 
 //Main
@@ -53,20 +61,23 @@ void randomEncounter() {
 			bossEncounter();
 			break;
 		}
-	case 2: 
+	case 2:
 		mobEncounter();
 		break;
-	case 4: 
+	case 4:
 		dungeonDepth();
-		roomTrack = 0; 
+		roomTrack = 0;
 		break;
 	}
 }
 
 void beginningAdventure() {
-	cout << "You stand in front of the entrance to the dungeon, you want to go inside.\n" << endl;
+	cout << "====================================================\n" << endl << 
+		"You stand in front of the entrance to the dungeon..." << endl << 
+		"You want to go inside...\n" << endl;
 	cout << "[1] Enter Dungeon" << endl <<
 		"[0] Exit Game\n" << endl <<
+		"====================================================\n" << endl <<
 		"Input value of your decision:" << endl;
 
 	cin >> cmd;
@@ -96,6 +107,7 @@ void beginningMenu() {
 
 	switch (cmd) {
 	case 1:
+		characterInitialize();
 		beginningAdventure();
 		break;
 	case 0:
@@ -115,4 +127,53 @@ void dungeonDepth() {
 
 void shopEncounter() {
 
+}
+
+void characterInitialize() {
+	int characterTypeHP[] = {15, 10, 20};
+	int characterTypeAtk[] = {5, 20, 10};
+	cout << "=============================\n" << endl <<
+		"What is your name adventurer?\n " << endl <<
+		"=============================\n" << endl <<
+		"Your name:" << endl;
+
+	cin >> pName;
+	system("cls");
+	do {
+		cout << "==============\n" << endl <<
+			"And you are...\n" << endl <<
+			"[1] a human..." << endl <<
+			"[2] an elf..." << endl <<
+			"[3] an orc...\n" << endl <<
+			"==============\n" << endl;
+		cout << "Input value of your decision:" << endl;
+
+		cin >> cmd;
+		system("cls");
+
+		switch (cmd)
+		{
+		case 1:
+			confirmCharacter();
+			break;
+		case 2:
+			confirmCharacter();
+			break;
+		case 3:
+			confirmCharacter();
+			break;
+		}
+	} while (!confirmCharacter);
+}
+
+void confirmCharacter() {
+	char ch; 
+	cout << "===================\n" << endl <<
+		"Are you sure? (Y/N)\n" << endl <<
+		"===================\n" << endl <<
+		"Input value of your decision:" << endl;
+	cin >> ch;
+	ch == 'y' && ch == 'Y' ? characterConfirm = true : characterConfirm = false;
+
+	system("cls");
 }
